a06ce1ef07d71b21a86d21571512b85cd96e8ab6
Ivan Makarov <vanomak@gmail.com>
2022-06-16 20:20:24 +1000

    Убрали лишние модули ML_Kit

--- lib/text-recognition.dart
+++ lib/text-recognition.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:google_ml_kit/google_ml_kit.dart';
+import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:url_launcher/url_launcher.dart';
 
